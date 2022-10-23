17156b6a61c48434b48c697e9506de6153ab22b6
Waseem Khalid <waseemsabra2@gmail.com>
2022-10-23 15:27:03 +0300

    value By Value

--- passingParameters/Program.cs
+++ passingParameters/Program.cs
@@ -2,8 +2,8 @@
 
 int x = 1;
 Console.WriteLine($"Before calling ValByVal, x = {x}");
-//method1.ValByVal( x); //value by value
-method1.ValByVal(ref x); //value by referance 
+method1.ValByVal( x); //value by value
+//method1.ValByVal(ref x); //value by referance 
 Console.WriteLine($"After calling ValByVal, x = {x}");
 
 
@@ -15,9 +15,10 @@ Console.ReadLine();
 
 public static class method1
 {
-    public static void ValByVal(ref int x)
+    public static void ValByVal(int x)
     {
         x++;
+        Console.WriteLine("x =="+ x);
     }
 
     
